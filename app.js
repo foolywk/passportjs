ebb4fbc9c607a21dcd534e1606b70f3f83985045
Brandon Foo <takkun00@gmail.com>
2014-03-10 20:21:57 -0700

    fb authentication working

--- app.js
+++ app.js
@@ -4,46 +4,80 @@ var http = require('http');
 var express = require('express');
 var routes = require('./routes');
 var path = require('path');
+var app = express();
+var config = require('./oauth.js')
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
-// main config
+// serialize and deserialize
+passport.serializeUser(function(user, done) {
+done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+done(null, obj);
+});
+
+// config
+passport.use(new FacebookStrategy({
+ clientID: config.facebook.clientID,
+ clientSecret: config.facebook.clientSecret,
+ callbackURL: config.facebook.callbackURL
+},
+function(accessToken, refreshToken, profile, done) {
+ process.nextTick(function () {
+   return done(null, profile);
+ });
+}
+));
+
 var app = express();
-app.set('port', process.env.PORT || 1337);
+
+app.configure(function() {
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.set('view options', { layout: false });
 app.use(express.logger());
+app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
+app.use(express.session({ secret: 'my_precious' }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.use(express.static(__dirname + '/public'));
 });
-
-app.configure('production', function(){
-    app.use(express.errorHandler());
-});
-
-// passport config
-var Account = require('./models/account');
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
-
-// mongoose
-mongoose.connect('mongodb://localhost/passport_local_mongoose');
 
 // routes
-require('./routes')(app);
-
-app.listen(app.get('port'), function(){
-  console.log(("Express server listening on port " + app.get('port')))
+app.get('/', routes.index);
+app.get('/ping', routes.ping);
+app.get('/account', ensureAuthenticated, function(req, res){
+res.render('account', { user: req.user });
 });
+
+app.get('/', function(req, res){
+res.render('login', { user: req.user });
+});
+
+app.get('/auth/facebook',
+passport.authenticate('facebook'),
+function(req, res){
+});
+app.get('/auth/facebook/callback',
+passport.authenticate('facebook', { failureRedirect: '/' }),
+function(req, res) {
+ res.redirect('/account');
+});
+app.get('/logout', function(req, res){
+req.logout();
+res.redirect('/');
+});
+
+// port
+app.listen(1337);
+
+// test authentication
+function ensureAuthenticated(req, res, next) {
+if (req.isAuthenticated()) { return next(); }
+res.redirect('/')
+}
--- models/account.js
+++ models/account.js
@@ -1,14 +1,10 @@
-// sample model
-
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var passportLocalMongoose = require('passport-local-mongoose');
 
 var Account = new Schema({
-  nickname: String,
-  birthdate: Date
+  username: String,
+  email: String,
+  time: {type: Date, default: Date.now}
 });
-
-Account.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('Account', Account);
