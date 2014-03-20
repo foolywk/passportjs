d6882312ec376f07bbb3e4f8fa74202e5135b1e1
Brandon Foo <takkun00@gmail.com>
2014-03-20 15:32:40 -0700

    added google strategy to authentication.js

--- authentication.js
+++ authentication.js
@@ -26,6 +26,34 @@ passport.use(new FacebookStrategy({
 },
 
 function(accessToken, refreshToken, profile, done) {
+  User.findOne({ oauthID: profile.id }, function(err, user) {
+   if(err) { console.log(err); }
+   if (!err && user != null) {
+     done(null, user);
+   } else {
+     var user = new User({
+       oauthID: profile.id,
+       name: profile.displayName,
+       created: Date.now()
+     });
+     user.save(function(err) {
+       if(err) {
+         console.log(err);
+       } else {
+         console.log("saving user ...");
+         done(null, user);
+       };
+     });
+   };
+  });
+  }
+));
+
+passport.use(new GoogleStrategy({
+ returnURL: config.google.returnURL,
+ realm: config.google.realm
+},
+function(accessToken, refreshToken, profile, done) {
 User.findOne({ oauthID: profile.id }, function(err, user) {
  if(err) { console.log(err); }
  if (!err && user != null) {
--- routes/index.js
+++ routes/index.js
@@ -4,35 +4,46 @@ var fs = require('fs');
 var User = require('../user.js');
 var passport = require('../authentication.js').passport
 var clientSecrets = require('../client_secrets.json')
-var auth;
 
-exports.index = function(req, res){
-  res.render('index', { title: "Start Bootstrap"});
+var OAuth2 = googleapis.auth.OAuth2;
+var test = new OAuth2("347150572630.apps.googleusercontent.com", "W0hjveoJ7dx4Gw_21fgou9Hw", "http://127.0.0.1:1337/auth/google/callback");
+
+exports.index = function (req, res) {
+    res.render('index', {
+        title: "Start Bootstrap"
+    });
 };
 
-exports.ping = function(req, res){
-  res.send("pong!", 200);
+exports.ping = function (req, res) {
+    res.send("pong!", 200);
 };
 
-exports.uploadVideo = function(req, res) {
-  console.log('## User object:', req.user);
-  console.log('## Client ID:', clientSecrets.web.client_id);
-  res.send(200);
+exports.uploadVideo = function (req, res) {
+    console.log('## User object:', req.user);
+    console.log('## Client ID:', clientSecrets.web.client_id);
+    res.send(200);
 
-  googleapis.discover('youtube','v3').withAuthClient(clientSecrets.web.client_id).execute(function(err,client) {
+    googleapis.discover('youtube', 'v3').execute(function (err, client) {
 
-    var metadata= {
-      snippet: {title:'title', description: 'description'},
-      status: {privacyStatus: 'privacy' }
-    };
+        var metadata = {
+            snippet: {
+                title: 'title',
+                description: 'description'
+            },
+            status: {
+                privacyStatus: 'privacy'
+            }
+        };
 
-    client
-      .youtube.videos.insert({ part: 'snippet,status'}, metadata)
-      .withMedia('video/MOV', fs.readFileSync(__dirname + '/test.MOV'))
-//      .withAuthClient(req.user.oauthID)
-      .execute(function(err, result) {
-          if (err) console.log(err);
-          else console.log(JSON.stringify(result, null, '  '));
-      });
-  });
+        if (!req.user) { return res.redirect('/login')}
+        else { test.credentials = { access_token: req.user.oauthID }}
+
+
+        client.youtube.videos.insert({
+            part: 'snippet,status'
+        }, metadata).withMedia('video/MOV', fs.readFileSync(__dirname + '/test.MOV')).withAuthClient(test).execute(function (err, result) {
+            if (err) console.log(err);
+            else console.log(JSON.stringify(result, null, ' '));
+        });
+    });
 };
