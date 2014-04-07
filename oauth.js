d29284d61bf83a6a772ef5ecd48166c69d5d9ea0
Brandon Foo <takkun00@gmail.com>
2014-04-07 15:19:54 -0700

    update callback uri

--- oauth.js
+++ oauth.js
@@ -2,7 +2,7 @@ var ids = {
 	facebook: {
 		clientID: '616700801754177',
 		clientSecret: 'cbcb065c56234d776bf328a4635b8200',
-		callbackURL: 'http://perfect-pitch.herokuapp.com/auth/facebook/callback'
+		callbackURL: 'http://localhost:1337/auth/facebook/callback'
 	},
 
 	google: {
