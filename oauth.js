f088d448883700dd5a38ee4150e6bbeba62030f1
Brandon Foo <takkun00@gmail.com>
2014-04-07 15:22:43 -0700

    still updating redirect uri

--- oauth.js
+++ oauth.js
@@ -2,7 +2,7 @@ var ids = {
 	facebook: {
 		clientID: '616700801754177',
 		clientSecret: 'cbcb065c56234d776bf328a4635b8200',
-		callbackURL: 'http://localhost:1337/auth/facebook/callback'
+		callbackURL: 'http://perfect-pitch.herokuapp.com/auth/facebook/callback'
 	},
 
 	google: {
